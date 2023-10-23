cbcedb09667506b84782dcf2a35d3c7b4537191a
samuel-bazinet <bazinet.samuel1@gmail.com>
2023-10-22 21:06:22 -0400

    Created PubSubConfigs for the server

---
+++ src/configs.rs
@@ -0,0 +1,22 @@
+use std::net::SocketAddr;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct PubSubConfigs {
+    pub addr: SocketAddr,
+}
+
+#[cfg(test)]
+mod tests {
+    use std::net::{SocketAddr, SocketAddrV4, Ipv4Addr};
+
+    use super::*;
+
+    /// Verifies that the PubSubConfigs can be built
+    #[test]
+    fn test_configs_build() {
+        let _ = PubSubConfigs {addr: SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8080))};
+        assert!(true);
+    }
+}
