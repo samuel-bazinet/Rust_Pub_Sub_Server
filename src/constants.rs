fb816a165a533582fa4235e55f4bc142e04a1b25
samuel-bazinet <bazinet.samuel1@gmail.com>
2023-10-22 22:09:16 -0400

    Created and started tests on setup_server function

---
+++ src/constants.rs
@@ -0,0 +1,8 @@
+
+#[cfg(test)]
+pub const TEST_RESOURCES_DIR_PATH: &str = "./test_resources/";
+
+#[cfg(test)]
+pub fn test_resource_path(resource_relative_path: &str) -> String {
+    TEST_RESOURCES_DIR_PATH.to_owned() + resource_relative_path
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,45 @@
-pub mod error_types;
-pub mod sub_list;
+use std::{fs::read_to_string, net::SocketAddr, str::FromStr, path::Path};
+
+mod configs;
+mod constants;
+mod error_types;
+mod sub_list;
+
+use configs::PubSubConfigs;
+use error_types::SetupErrors;
+
+use serde_json::Value;
 
 ///
 /// Sets up the server. Reads a JSON file where the app settings are defined.
 /// 
-pub fn setup_server() {
+pub fn setup_server<'a>(config_file: &str) -> Result<PubSubConfigs, SetupErrors> {
+    if let Ok(file_content) = read_to_string(config_file) {
+        if let Ok(config) = serde_json::from_str::<Value>(&file_content) {
+            Ok(PubSubConfigs { addr: SocketAddr::from_str(config["addr"].as_str().unwrap()).unwrap() })
+        } else {
+            Err(SetupErrors::CannotReadConfigFile)
+        }
+    } else {
+        Err(SetupErrors::CannotFindConfigFile)
+    }
+}
+
+///
+/// Starts listening to the configured port. 
+/// 
+pub fn start_listening() -> ! {
     todo!()
 }
 
-pub fn start_listening() {
-    todo!()
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+
+    #[test]
+    fn test_setup_server_returns_configs() {
+        let result = setup_server(constants::test_resource_path("test.json").as_str());
+        assert!(result.is_ok());
+    }
 }
