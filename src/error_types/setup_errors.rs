3f92936c97ec36fd3db5a6fd541dced68074f277
samuel-bazinet <bazinet.samuel1@gmail.com>
2023-10-22 22:08:10 -0400

    Added SetupErrors enum

---
+++ src/error_types/setup_errors.rs
@@ -0,0 +1,26 @@
+use std::{error::Error, fmt::Display};
+
+#[derive(Debug, PartialEq)]
+pub enum SetupErrors {
+    CannotReadConfigFile,
+    CannotFindConfigFile,
+    InvalidConfigFileFormat,
+    InvalidSocketAddress,
+}
+
+impl Error for SetupErrors {}
+
+impl Display for SetupErrors {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SetupErrors::CannotReadConfigFile => 
+                write!(f, "The config file cannot be read ."),
+            SetupErrors::CannotFindConfigFile => 
+                write!(f, "The path to the condifg file cannot be found."),
+            SetupErrors::InvalidConfigFileFormat => 
+                write!(f, "The config file was inproperly formated."),
+            SetupErrors::InvalidSocketAddress =>
+                write!(f, "The address in the config file is not formatted properly.")
+        }
+    }
+}
