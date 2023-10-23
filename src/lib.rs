3810998f2c8c09e12bba564df04e63977bab567d
samuel-bazinet <bazinet.samuel1@gmail.com>
2023-10-22 20:41:06 -0400

    Added a basic skeleton for the lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,13 @@
 pub mod error_types;
 pub mod sub_list;
+
+///
+/// Sets up the server. Reads a JSON file where the app settings are defined.
+/// 
+pub fn setup_server() {
+    todo!()
+}
+
+pub fn start_listening() {
+    todo!()
+}
