a001e6bd43a70c850a78b9787439b8e151fdb5be
samuel-bazinet <bazinet.samuel1@gmail.com>
2023-11-16 12:06:57 -0500

    Completed MVP

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,13 @@ mod configs;
 mod constants;
 mod error_types;
 mod sub_list;
+mod message_processor;
 
-use std::net::UdpSocket;
+use std::{net::UdpSocket, sync::{Mutex, Arc}};
 
 pub use configs::setup_server;
 use error_types::ListeningErrors;
+use sub_list::SubscriptionManager;
 
 ///
 /// Starts listening to the configured port.
@@ -15,18 +17,20 @@ pub fn start_listening(config: configs::PubSubConfigs) -> Result<(), ListeningEr
     let mut error;
     if let Ok(udp_socket) = UdpSocket::bind(config.addr) {
         let mut buffer = [0u8;constants::BUFFER_SIZE];
+        let subscription_manager = Arc::new(Mutex::new(SubscriptionManager::new())); 
         loop {
             if let Ok((size, src)) = udp_socket.recv_from(&mut buffer) {
                 let partial_buf = &buffer[..size];
+                message_processor::process_message(partial_buf, src, &mut subscription_manager.lock().unwrap());
                 todo!("Make function to parse bytes and determine if message or new subscriptions. Will need a thread pool.")
             } else {
                 error = ListeningErrors::UnableToReceive;
-                todo!("Add a logger to log the error")
+                todo!("Add a logger to log the error then exit the program")
             }
         }
     } else {
         error = ListeningErrors::UnableToBind;
-        todo!("Add a logger to log the error")
+        todo!("Add a logger to log the error then exit the program")
     }
 }
 
---
+++ src/message_processor.rs
@@ -0,0 +1,31 @@
+
+use rpss_utils::server_ops;
+
+use std::net::{SocketAddr, UdpSocket};
+
+use crate::sub_list::SubscriptionManager;
+
+pub fn process_message(message: &[u8], src: SocketAddr, sub_manager: &mut SubscriptionManager) {
+    if server_ops::is_update_subscriptions(&message[0..8]) {
+        for message_id_bytes in message[8..].chunks(8) {
+            if let Ok(message_id_bytes) = message_id_bytes.try_into() {
+                if let Err(error) = sub_manager.add_subscription(usize::from_le_bytes(message_id_bytes), src) {
+                    todo!("Deal with error")
+                }
+            } else {
+                todo!("Make error for invalid conversion")
+            }
+            
+        }
+    } else {
+        if let Ok(message_id_bytes) = message[0..8].try_into() {
+            if let Ok(subscribers) = sub_manager.get_subscribers(usize::from_le_bytes(message_id_bytes)) {
+                let udp_sender = UdpSocket::bind("0.0.0.0:0").unwrap();
+                for subscriber in subscribers {
+                    let result = udp_sender.send_to(&message[8..], subscriber);
+                }
+            }
+
+        }
+    }
+}
